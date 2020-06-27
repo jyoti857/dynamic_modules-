716e34d29ae2197b0a736ad56cf6418e1dbcf6b6
Jyoti RS <jyotiranjans@cnxsi.com>
2020-06-27 15:16:36 +0530

    opp card

--- src/containers/Opportunity/PrimaryScreen/components/OpportunityDashboard/DashboardFlatList/OpportunityCard.js
+++ src/containers/Opportunity/PrimaryScreen/components/OpportunityDashboard/DashboardFlatList/OpportunityCard.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {View} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import {Card, Subheading} from 'react-native-paper';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const OpportunityCard = props => {
-  return <View />;
+  const {data} = props;
+  return (
+    <Animatable.View>
+      <Card>
+        <Card.Title
+          title={<Subheading>{data.name}</Subheading>}
+          right={() => <FontAwesome name="fire" size={18} color="yellow" />}
+        />
+      </Card>
+    </Animatable.View>
+  );
 };
 
 export default OpportunityCard;
